831da833cb63589905f01477cab8c8ec0e8d1467
hassanisk <hiskaf1@gmail.com>
2017-04-03 10:01:54 +0100

    first edition

--- js/app.js
+++ js/app.js
@@ -1,1 +1,33 @@
-alert();
+
+let duck;
+let gameTimer;
+let output;
+
+
+function init(){
+  duck = document.getElementById('duck');
+  gameTimer = setInterval(gameLoop, 50);
+  output = document.getElementById('output');
+  playDuck();
+}
+
+function playDuck(){
+  var x =Math.floor(Math.random()*600);
+  var y =Math.floor(Math.random()*700);
+  duck.style.left = '400px';
+  duck.style.top= y +'px';
+
+}
+function gameLoop(){
+
+  var y = parseInt(duck.style.top)-10;
+  if (y<-50){
+    playDuck();
+  }else{
+    duck.style.top= y + 'px';
+  }
+}
+
+function hitDuck (){
+  playDuck();
+}

a1e676e7f576f34a55c6e2467b8b07a8659d83b3
hassanisk <hiskaf1@gmail.com>
2017-04-03 23:54:10 +0100

    duck randmly moving

--- js/app.js
+++ js/app.js
@@ -1,33 +1,54 @@
+$(function(){
 
-let duck;
-let gameTimer;
-let output;
+  animateDiv();
+  $('#target').click(function(){
+    $(this).hide();
+  });
+});
 
 
-function init(){
-  duck = document.getElementById('duck');
-  gameTimer = setInterval(gameLoop, 50);
-  output = document.getElementById('output');
-  playDuck();
+function makeNewPosition(){
+
+  // Get viewport dimensions (remove the dimension of the div)
+  var height = $(window).height()-100;
+  var width = $(window).width() - 100;
+
+  var nheight = Math.floor(Math.random() * height);
+  var nwidth = Math.floor(Math.random() * width);
+
+  return [nheight,nwidth];
+
 }
 
-function playDuck(){
-  var x =Math.floor(Math.random()*600);
-  var y =Math.floor(Math.random()*700);
-  duck.style.left = '400px';
-  duck.style.top= y +'px';
+function animateDiv(){
+
+  var newq = makeNewPosition();
+  var oldq = $('.duck').offset();
+  var speed = calcSpeed([oldq.top, oldq.left], newq);
+  // $('duck').style.left =  '650px';
+  // $('duck').style.top= '200px';
+
+  $('.duck').animate({ top: newq[0], left: newq[1] }, speed, function(){
+    animateDiv();
+  });
+
+
+}
+
+function calcSpeed(prev, next) {
+
+  var x = Math.abs(prev[1] - next[1]);
+  var y = Math.abs(prev[0] - next[0]);
+
+  var greatest = x > y ? x : y;
+
+  var speedModifier = 1;
+
+  var speed = Math.ceil(greatest/speedModifier);
+
+  return speed;
 
 }
 function gameLoop(){
 
-  var y = parseInt(duck.style.top)-10;
-  if (y<-50){
-    playDuck();
-  }else{
-    duck.style.top= y + 'px';
-  }
-}
-
-function hitDuck (){
-  playDuck();
 }
